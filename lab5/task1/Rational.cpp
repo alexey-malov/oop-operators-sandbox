804ae6e07ccc71efccd473852a4e6589e9d0d384
Und3rMySk1n <undermyskin@inbox.ru>
2016-01-16 11:27:51 +0300

    Рациональное число имеет унарные операторы "+" и "-"

--- lab5/task1/Rational.cpp
+++ lab5/task1/Rational.cpp
@@ -55,8 +55,15 @@ unsigned GCD(unsigned a, unsigned b)
 // TODO: 2. Реализовать унарный + и унарный -
 //////////////////////////////////////////////////////////////////////////
 
+CRational const CRational::operator-() const
+{
+	return CRational(-m_numerator, m_denominator);
+}
 
-
+CRational const CRational::operator+() const
+{
+	return CRational(m_numerator, m_denominator);
+}
 
 //////////////////////////////////////////////////////////////////////////
 // TODO: 3. Реализовать бинарный +
